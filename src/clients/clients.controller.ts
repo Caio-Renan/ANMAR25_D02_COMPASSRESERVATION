882c1633eee4dfaca5b0cf470e565bf0bbd59f84
Marcelo Plínio <mplmdswork@gmail.com>
2025-04-30 16:40:24 -0300

    feat(controller): Add Get method by FindAll and FindOne

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -3,14 +3,25 @@ import { CreateClientDto } from './dto/create-client.dto';
 import { ClientsService } from './clients.service';
 import { Param, Patch } from '@nestjs/common';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { Get } from '@nestjs/common';
 
-@Controller('clients') 
+@Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
 
   @Post()
   async create(@Body() createClientDto: CreateClientDto) {
     return this.clientsService.create(createClientDto);
+  }
+
+  @Get()
+  async findAll() {
+    return this.clientsService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.clientsService.findOne(Number(id));
   }
 
   @Patch(':id')
@@ -20,7 +31,4 @@ export class ClientsController {
   ) {
     return this.clientsService.update(Number(id), updateClientDto);
   }
-
 }
-
-
