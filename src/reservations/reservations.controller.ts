254e4d3954069d27121b5cb3d42aa2e1622feec7
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-04-30 15:15:55 -0300

    feat: adding logic to the reservation controller

--- src/reservations/reservations.controller.ts
+++ src/reservations/reservations.controller.ts
@@ -1,19 +1,36 @@
-import { Controller, Delete, Get, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Post } from "@nestjs/common";
 import { ReservationService } from "./reservations.service";
+import { CreateReservationDto } from "./dto/create-reservation-dto";
 
 
 @Controller('reservations')
-export class ReservationController{
+export class ReservationController {
 
-     constructor(private readonly reservationService: ReservationService) {}
+     constructor(private readonly reservationService: ReservationService) { }
 
      @Post()
-     
+     async create(@Body() data: CreateReservationDto) {
+          return this.reservationService.create(data);
+     }
+
      @Get()
+     async findAll() {
+          return this.reservationService.list();
+     }
 
      @Get('id')
+     async findOne(@Param() id: number) {
+          return this.reservationService.findOne(id);
+     }
 
      @Put('id')
+     async update(@Param() id: number) {
+          return this.reservationService.update(id);
+     }
 
      @Delete('id')
+     async delete(@Param() id: number) {
+          return this.reservationService.delete(id);
+     }
+
 }
