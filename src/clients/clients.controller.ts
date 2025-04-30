0d5b6b07a7e112cfa1d5da97a1701c0217cebdc3
Marcelo Plínio <mplmdswork@gmail.com>
2025-04-30 16:27:31 -0300

    feat(controller): Add a patch method

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,6 +1,8 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { ClientsService } from './clients.service';
+import { Param, Patch } from '@nestjs/common';
+import { UpdateClientDto } from './dto/update-client.dto';
 
 @Controller('clients') 
 export class ClientsController {
@@ -10,4 +12,15 @@ export class ClientsController {
   async create(@Body() createClientDto: CreateClientDto) {
     return this.clientsService.create(createClientDto);
   }
+
+  @Patch(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateClientDto: UpdateClientDto,
+  ) {
+    return this.clientsService.update(Number(id), updateClientDto);
+  }
+
 }
+
+
