d54aa58b8a255e4ccfdf536d0c613636f191f8e2
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-01 02:26:52 -0300

    feat: add reservationValidationService as provider

--- src/reservations/reservations.module.ts
+++ src/reservations/reservations.module.ts
@@ -2,11 +2,12 @@ import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { ReservationController } from "./reservations.controller";
 import { ReservationService } from "./reservations.service";
+import { ReservationValidationService } from "./reservationsValidate.service";
 
 @Module({
      imports: [PrismaModule],
      controllers: [ReservationController],
-     providers: [ReservationService],
+     providers: [ReservationService, ReservationValidationService],
      exports: [ReservationService]
 })
 export class ReservationModule {
