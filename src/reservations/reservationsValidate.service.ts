6beacbbb5487f61c56dcba3eca1eac69e7173893
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-01 02:19:10 -0300

    feat: create isSpaceActive method validation

--- src/reservations/reservationsValidate.service.ts
+++ src/reservations/reservationsValidate.service.ts
@@ -7,6 +7,14 @@ import { PrismaService } from "src/prisma/prisma.service";
 @Injectable()
 export class ReservationValidationService {
      constructor(private readonly prisma: PrismaService) { }
-  
 
+
+     async isSpaceActive(spaceId: number): Promise<boolean> {
+          const space = await this.prisma.space.findUnique({
+               where: { id: spaceId },
+               select: { status: true },
+          });
+
+          return space?.status === 'ACTIVE'
+     }
 }
