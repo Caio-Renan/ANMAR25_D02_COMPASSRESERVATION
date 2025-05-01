2c84cfb043c0eebeb4d8f2728e6f9a11bb77cce4
Caio Renan <caiorenan016@gmail.com>
2025-05-01 17:19:50 -0300

    feat(users): implement initial setup for users controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,36 @@
+import { Body, Controller, Get, Patch, Post, Query, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdateUserDTO } from './dto/update-user.dto'
+import { FilterUserDTO } from './dto/filter-user.dto'
+import { ParamId } from '../common/decorators/param-id.decorator'
+
+@Controller('users')
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+  
+    @Post()
+    async create(@Body() dto: CreateUserDTO) {
+      return this.usersService.create(dto);
+    }
+  
+    @Patch(':id')
+    async updatePartial(@ParamId() id: number, @Body() dto: UpdateUserDTO) {
+      return this.usersService.update(id, dto);
+    }
+  
+    @Get()
+    async findAll(@Query() filter: FilterUserDTO) {
+      return this.usersService.findAll(filter);
+    }
+  
+    @Get(':id')
+    async findOne(@ParamId() id: number) {
+      return this.usersService.findById(id);
+    }
+  
+    @Delete(':id')
+    async softDelete(@ParamId() id: number) {
+      return this.usersService.softDelete(id);
+    }
+}
