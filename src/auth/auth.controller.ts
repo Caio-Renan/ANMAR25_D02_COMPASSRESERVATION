adff30717290f7e69408b5141363febc93eb1dd3
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-01 03:15:53 -0300

    feat: create login and register structure

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,25 @@
-import { Body, Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 
 
 @Controller()
 export class AuthController{
+
+     constructor(
+          private readonly authService: AuthService
+     ) {}
+
+
+     @Post('login')
+     async login(@Body() email: string, password: string){
+          return { email, password };
+     }
+
+     @Post('register')
+     async register(@Body() body){
+          return { body }
+     }
+
+
 }
