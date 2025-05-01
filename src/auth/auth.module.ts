eb092d54f7ca63a3ea06fbcfd9dbe504277d4233
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-01 03:02:34 -0300

    feat: configure auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,20 @@
 import { Module } from "@nestjs/common";
+import { JwtModule } from "@nestjs/jwt";
+import { PrismaModule } from "src/prisma/prisma.module";
+import { AuthService } from "./auth.service";
+import { AuthController } from "./auth.controller";
 
 
-@Module()
+@Module({
+     imports: [JwtModule.register({
+          secret: process.env.JWT_SECRET,
+     }),
+     PrismaModule
+],
+     providers: [AuthService],
+     exports: [AuthService],
+     controllers: [AuthController],
+})
 export class AuthModule{
      
 }
