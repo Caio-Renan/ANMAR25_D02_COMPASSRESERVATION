e4259c639d9c099a3b406c4af5f54c8106f69e58
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-02 17:56:05 -0300

    refactor: move MailerModule to email.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,32 +6,9 @@ import { UsersModule } from './users/users.module';
 import { SpacesModule } from './spaces/spaces.module';
 import { ClientsModule } from './clients/clients.module';
 import { AuthModule } from './auth/auth.module';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com',
-        port: 587,
-        secure: false,
-        auth: {
-          user: process.env.MAIL_USER,
-          pass: process.env.MAIL_PASS,
-        },
-      },
-      defaults: {
-        from: '"nest-modules" <modules@nestjs.com>',
-      },
-      template: {
-        dir: __dirname + '/templates',
-        adapter: new PugAdapter(),
-        options: {
-          strict: true,
-        }
-      }
-    }),
     AuthModule,
     ReservationModule, 
     UsersModule, 
---
+++ src/email/email.module.ts
@@ -0,0 +1,33 @@
+import { MailerModule } from "@nestjs-modules/mailer";
+import { Module } from "@nestjs/common";
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
+
+
+@Module({
+     imports: [ MailerModule.forRoot({
+           transport: {
+             host: 'smtp.gmail.com',
+             port: 587,
+             secure: false,
+             auth: {
+               user: process.env.MAIL_USER,
+               pass: process.env.MAIL_PASS,
+             },
+           },
+           defaults: {
+             from: '"nest-modules" <modules@nestjs.com>',
+           },
+           template: {
+             dir: __dirname + '/templates',
+             adapter: new PugAdapter(),
+             options: {
+               strict: true,
+             }
+           }
+         }),],
+     providers: [],
+     exports: []
+})
+export class EmailModule {
+     
+}
