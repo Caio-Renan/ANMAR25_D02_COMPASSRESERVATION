732c74dfd6f8fd181bc2670229f11fafe08077f1
JeerTeles <jadersoncontatos@gmail.com>
2025-05-02 18:39:47 -0300

    importing the ResourcesModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ReservationModule } from './reservations/reservations.module';
 import { UsersModule } from './users/users.module';
 import { SpacesModule } from './spaces/spaces.module';
+import { ResourcesModule } from './resources/resources.module';
 
 @Module({
-  imports: [ReservationModule, UsersModule, SpacesModule],
+  imports: [ReservationModule, UsersModule, SpacesModule, ResourcesModule],
   controllers: [AppController],
   providers: [AppService],
 })
