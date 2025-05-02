b966d1b3c4b9739063e43f61602642ec02d8812f
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-02 13:36:09 -0300

    feat: create jwt expiration env variable

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -9,7 +9,7 @@ import { AuthController } from "./auth.controller";
      imports: [JwtModule.register({
           global: true,
           secret: process.env.JWT_SECRET,
-          signOptions: { expiresIn: '60s' }
+          signOptions: { expiresIn: process.env.JWT_EXPIRATION || '1d' }
      }),
      PrismaModule
 ],
