b81d83a953eba209e96a12369cf57a607d815ef5
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-02 13:15:51 -0300

    feat: sets the decorator of the current user

--- src/common/decorators/current-user.decorator.ts
+++ src/common/decorators/current-user.decorator.ts
@@ -1,9 +1,21 @@
-import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { createParamDecorator, ExecutionContext, NotFoundException } from "@nestjs/common";
 
 
 export const CurrentUser = createParamDecorator(
-     (_data: unknown, context: ExecutionContext) => {
+     (filter: string, context: ExecutionContext) => {
           const request = context.switchToHttp().getRequest();
+
+
+          if (filter) {
+               return request.user[filter];
+          }
+
+          if (!request.user) {
+               throw new NotFoundException('User not found in request. Use AuthGuard to get the user')
+          }
+
           return request.user;
+
+
      }
 );
