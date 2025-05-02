340f20aad458ee3be9c6bc8883f2449e1cfbe503
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-02 17:05:24 -0300

    feat: mailer setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,39 @@ import { ReservationModule } from './reservations/reservations.module';
 import { UsersModule } from './users/users.module';
 import { SpacesModule } from './spaces/spaces.module';
 import { ClientsModule } from './clients/clients.module';
+import { AuthModule } from './auth/auth.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
-  imports: [ReservationModule, UsersModule, SpacesModule, ClientsModule],
+  imports: [
+    MailerModule.forRoot({
+      transport: {
+        host: '',
+        port: 587,
+        secure: false,
+        auth: {
+          user: "username",
+          pass: "password",
+        },
+      },
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: process.cwd() + '/templates/',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        }
+      }
+    }),
+    AuthModule,
+    ReservationModule, 
+    UsersModule, 
+    SpacesModule, 
+    ClientsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
