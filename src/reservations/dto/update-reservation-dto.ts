1d55c407f3ba2274483f00af948f841ef5971423
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-05-02 11:42:50 -0300

    feat: update to reservation update dto

--- src/reservations/dto/update-reservation-dto.ts
+++ src/reservations/dto/update-reservation-dto.ts
@@ -1,4 +1,27 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateReservationDto } from './create-reservation-dto';
+import { Type } from 'class-transformer';
+import {
+  IsDate,
+  IsOptional,
+  IsString,
+  IsIn,
+  IsNotEmpty,
+} from 'class-validator';
 
-export class UpdateReservationDto extends PartialType(CreateReservationDto) {}
+export class UpdateReservationDto {
+  @IsOptional()
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
+  startDate?: Date;
+
+  @IsOptional()
+  @IsDate()
+  @IsNotEmpty()
+  @Type(() => Date)
+  endDate?: Date;
+
+  @IsOptional()
+  @IsString()
+  @IsIn(['OPEN', 'APPROVED', 'CLOSED', 'CANCELED'])
+  status?: string;
+}
