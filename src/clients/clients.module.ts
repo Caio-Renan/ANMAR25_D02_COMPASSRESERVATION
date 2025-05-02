f6d399b9d5eb93e4be9154e9de83543d4bcb4cdc
Marcelo Plínio <mplmdswork@gmail.com>
2025-05-02 14:56:10 -0300

    feat(clients): Add Prisma route to clients module

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { ClientsController } from './clients.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ClientsController],
   providers: [ClientsService, PrismaService],
 })
