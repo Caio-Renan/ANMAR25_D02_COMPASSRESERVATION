f4c16429700ef4199c561f417e033f5aa92be39c
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-01 22:48:51 -0300

    feat: create checktoken and register methods at auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,47 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
 import { PrismaService } from "src/prisma/prisma.service";
+import { UsersService } from "src/users/users.service";
+import { AuthRegisterDto } from "./dto/auth-register.dto";
 
 
 
 @Injectable()
 export class AuthService {
+
+     private issuer = 'login';
+     private audience = 'users';
+
      constructor(
           private readonly prisma: PrismaService,
-          private readonly jwtService: JwtService
+          private readonly jwtService: JwtService,
+          private readonly userService: UsersService,
      ) { }
+
+     private createToken(user: { id: number; name: string; email: string }) {
+          return {
+               accessToken: this.jwtService.sign(
+                    {
+                         id: user.id,
+                         name: user.name,
+                         email: user.email,
+                    },
+                    {
+                         expiresIn: '2 days',
+                         subject: String(user.id),
+                         issuer: this.issuer,
+                         audience: this.audience,
+                    },
+               ),
+          };
+     }
+
+
+     async register(dto: AuthRegisterDto) {
+
+          const user = await this.userService.create(dto);
+
+          return this.createToken(user);
+     }
+
 }
