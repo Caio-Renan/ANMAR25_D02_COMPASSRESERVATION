83ce2c41a68230b91f849a73a9cebe5af3e646e8
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-03 17:01:18 -0300

    feat: verify email credentials

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -5,34 +5,40 @@ import { EmailService } from "./email.service";
 
 
 @Module({
-     imports: [ 
-          MailerModule.forRootAsync({
-          useFactory: () => ({
-           transport: {
-             host: 'smtp.gmail.com',
-             port: 587,
-             secure: false,
-             auth: {
-               user: process.env.MAIL_USER,
-               pass: process.env.MAIL_PASS,
-             },
-           },
-           defaults: {
-             from: '"nest-modules" <modules@nestjs.com>',
-           },
-           template: {
-             dir: __dirname + '/common' + '/templates',
-             adapter: new PugAdapter(),
-             options: {
-               strict: true,
-             }
-           }
-         })
-     }),
-],
-     providers: [EmailService],
-     exports: [EmailService],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => {
+        if (!process.env.MAIL_USER || !process.env.MAIL_PASS) {
+          console.warn('Email credentials are missing. Email module will not be initialized.');
+          return {};
+        }
+        return {
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+              user: process.env.MAIL_USER,
+              pass: process.env.MAIL_PASS,
+            },
+          },
+          defaults: {
+            from: '"nest-modules" <modules@nestjs.com>',
+          },
+          template: {
+            dir: __dirname + '/common' + '/templates',
+            adapter: new PugAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      }
+    }),
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
 })
 export class EmailModule {
-     
+
 }
