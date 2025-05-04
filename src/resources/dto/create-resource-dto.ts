4b4d2f4d1f547fcd32668c3f7a78b8d151aa4b80
JeerTeles <jadersoncontatos@gmail.com>
2025-05-04 19:54:16 -0300

    feature: dto validations: MaxLength and MinLength

--- src/resources/dto/create-resource-dto.ts
+++ src/resources/dto/create-resource-dto.ts
@@ -1,14 +1,17 @@
-import { IsNotEmpty, IsString, IsOptional, IsInt, Min }
+import { IsNotEmpty, IsString, IsOptional, IsInt, Min, MaxLength, MinLength }
 from 'class-validator';
 
 export class CreateResourceDto {
   @IsNotEmpty()
   @IsString()
+  @MaxLength(60)
+  @MinLength(3)
   name: string;
 
   @IsOptional()
   @IsString()
-  description?: string;
+  @MaxLength(60)
+  description: string;
 
   @IsNotEmpty()
   @IsInt()
