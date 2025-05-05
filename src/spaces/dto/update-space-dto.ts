0d78f4e00ff0710c2801e6d7a9671e92cca7ab69
Caio Renan <caiorenan016@gmail.com>
2025-05-05 02:59:50 -0300

    refactor(spaces): clean up unnecessary code in UpdateSpaceDto

--- src/spaces/dto/update-space-dto.ts
+++ src/spaces/dto/update-space-dto.ts
@@ -1,19 +1,4 @@
-import { IsOptional, IsString, IsInt, Min } from "class-validator";
 import { createSpaceDto } from "./create-space-dto";
 import { PartialType } from "@nestjs/mapped-types";
 
-export class updateSpaceDto extends PartialType (createSpaceDto) {
-  
-  @IsString()
-  @IsOptional()
-  name?: string;
-
-  @IsString()
-  @IsOptional()
-  description?: string;
-
-  @IsInt()
-  @Min(1, { message: "Capacity must be at least 1" })
-  @IsOptional()
-  capacity?: number;
-}
+export class updateSpaceDto extends PartialType (createSpaceDto) {}
