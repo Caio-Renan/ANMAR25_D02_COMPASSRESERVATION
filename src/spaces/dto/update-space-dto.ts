82d668edb237e6c7bc81287d4ead56718f7dd0b6
Caio Renan <caiorenan016@gmail.com>
2025-05-05 03:13:36 -0300

    refactor(spaces): rename updateSpaceDto to UpdateSpaceDto for consistency

--- src/spaces/dto/update-space-dto.ts
+++ src/spaces/dto/update-space-dto.ts
@@ -1,4 +1,4 @@
 import { CreateSpaceDto } from "./create-space-dto";
 import { PartialType } from "@nestjs/mapped-types";
 
-export class updateSpaceDto extends PartialType (CreateSpaceDto) {}
+export class UpdateSpaceDto extends PartialType (CreateSpaceDto) {}
