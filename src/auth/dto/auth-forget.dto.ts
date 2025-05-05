fb2040db1a3fc3b9e158af1b188b20b749843fee
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-04 22:20:51 -0300

    refactor: changing auth dtos

--- src/auth/dto/auth-forget.dto.ts
+++ src/auth/dto/auth-forget.dto.ts
@@ -1,8 +1,12 @@
-import { IsEmail } from "class-validator";
+import { Transform } from "class-transformer";
+import { IsEmail, IsString, MaxLength } from "class-validator";
 
 
 export class AuthForgetDto {
 
      @IsEmail()
+     @IsString()
+     @MaxLength(150)
+     @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
      email: string;
 }
