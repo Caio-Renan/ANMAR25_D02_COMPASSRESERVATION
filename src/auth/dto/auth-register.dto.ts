c1d643288bd6989257ee3fe6192db0d4dbac2f41
Caio Renan <caiorenan016@gmail.com>
2025-05-06 02:03:46 -0300

    refactor(auth): update to reflect variable renaming from DTO to Dto

--- src/auth/dto/auth-register.dto.ts
+++ src/auth/dto/auth-register.dto.ts
@@ -1,5 +1,5 @@
-import { CreateUserDTO } from "src/users/dto/create-user.dto";
+import { CreateUserDto } from "src/users/dto/create-user.dto";
 
 
-export class AuthRegisterDto extends CreateUserDTO {
+export class AuthRegisterDto extends CreateUserDto {
 }
