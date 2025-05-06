972c8d614982666648dd5ab51ec091196c4898b8
Marcelo Plínio <mplmdswork@gmail.com>
2025-05-06 16:27:07 -0300

    refactor(env-dto): update EnvironmentDto to remove EMAIL_ENABLED and add email validation directly

--- src/common/config/env/env.dto.ts
+++ src/common/config/env/env.dto.ts
@@ -13,16 +13,11 @@ export class EnvironmentDto {
   @IsUrl({require_tld: false}, { message: 'APP_URL must be a valid URL (e.g., http://localhost:3000 or https://meuapp.com)' })
   APP_URL: string;
 
-  @IsBooleanString({ message: 'EMAIL_ENABLED must be "true" or "false"' })
-  EMAIL_ENABLED: string;
-
-  @ValidateIf(o => o.EMAIL_ENABLED === "true")
   @IsEmail({}, { message: 'MAIL_USER must be a valid email address' })
-  MAIL_USER?: string;
-
-  @ValidateIf(o => o.EMAIL_ENABLED === "true")
+  MAIL_USER: string;
+  
   @IsNotEmpty({ message: 'MAIL_PASS is required' })
-  MAIL_PASS?: string;
+  MAIL_PASS: string;
 
   @IsNumberString()
   PORT: string;
