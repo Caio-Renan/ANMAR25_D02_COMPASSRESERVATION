6c055eb25b0c7756e65ab2f5b16c4f5a91e85e84
Caio Renan <caiorenan016@gmail.com>
2025-05-06 06:27:45 -0300

    chore(users): add UserValidateService provider to module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { AuthModule } from 'src/auth/auth.module';
+import { UserValidationService } from './usersValidate.service';
 
 @Module({
   imports: [PrismaModule, forwardRef(() =>AuthModule)],
   controllers: [UsersController],
-  providers: [UsersService],
+  providers: [UsersService, UserValidationService],
   exports: [UsersService]
 })
 export class UsersModule {}
