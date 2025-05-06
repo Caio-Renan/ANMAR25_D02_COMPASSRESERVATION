6bc8c964669b2b0207412409b3c539c5cd552d97
Caio Renan <caiorenan016@gmail.com>
2025-05-06 00:49:22 -0300

    refactor(users): rename variables from DTO to Dto for consistency

--- src/users/dto/filter-user.dto.ts
+++ src/users/dto/filter-user.dto.ts
@@ -2,7 +2,7 @@ import { IsOptional, IsEnum } from 'class-validator';
 import { Status } from '@prisma/client';
 import { PaginationDto } from '../../common/dto/pagination.dto'
 import { IsPersonName, IsCustomEmail } from 'src/common/decorators';
-export class FilterUserDTO extends PaginationDto {
+export class FilterUserDto extends PaginationDto {
   @IsOptional()
   @IsPersonName()
   name?: string;
