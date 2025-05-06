5f91c6b9ecdd8c367d80d621956508a178709790
Caio Renan <caiorenan016@gmail.com>
2025-05-06 07:11:23 -0300

    chore(resources): add resourcesValidateService provider to module

--- src/resources/resources.module.ts
+++ src/resources/resources.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ResourcesController } from './resources.controller';
 import { ResourcesSevice } from './resources.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ResourceValidationService } from './resourcesValidate.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ResourcesController],
-  providers: [ResourcesSevice],
+  providers: [ResourcesSevice, ResourceValidationService],
 })
 export class ResourcesModule {}
