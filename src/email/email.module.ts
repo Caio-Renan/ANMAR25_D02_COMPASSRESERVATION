a2fb5a38c31271a763ce8ac5494d9aa02aba1f5c
Thiago Sampaio <thiago.sampaiog@gmail.com>
2025-05-06 15:30:00 -0300

    refactor(email): add dot env variables

--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -14,8 +14,8 @@ import { join } from 'path';
         }
         return {
           transport: {
-            host: 'smtp.gmail.com',
-            port: 587,
+            host: process.env.MAIL_HOST,
+            port: Number(process.env.MAIL_PORT),
             secure: false,
             auth: {
               user: process.env.MAIL_USER,
