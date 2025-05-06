2f7fbc05f26e2371bb7d156645a95f439e556b24
Caio Renan <caiorenan016@gmail.com>
2025-05-06 07:47:40 -0300

    feat(spaces): create async validations in spacesValidate.service

---
+++ src/spaces/spacesValidate.service.ts
@@ -0,0 +1,27 @@
+import {
+    Injectable,
+    NotFoundException,
+    ConflictException,
+  } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateSpaceDto } from './dto/update-space.dto';
+import { Space } from '@prisma/client';
+
+@Injectable()
+export class SpaceValidationService {
+    constructor(private readonly prisma: PrismaService) {}
+    async validateSpaceFields(dto: UpdateSpaceDto) {
+        if (dto.name) {
+          const nameExists = await this.prisma.space.findUnique({ where: { name: dto.name } });
+          if (nameExists) throw new ConflictException('Space already registered');
+        }
+      }
+    
+     async getSpaceOrFail(id: number): Promise<Space> {
+        const space = await this.prisma.space.findUnique({ where: { id } });
+    
+        if (!space) throw new NotFoundException('Space not found');
+    
+        return space;
+      }
+}
