f59fb1875e0719d7e77f4db57fe90923da8ad0d5
Caio Renan <caiorenan016@gmail.com>
2025-05-06 05:07:10 -0300

    refactor(auth): apply custom validation decorators to AuthLoginDto

--- src/auth/dto/auth-login.dto.ts
+++ src/auth/dto/auth-login.dto.ts
@@ -1,19 +1,9 @@
-import { Transform } from "class-transformer";
-import { IsEmail, IsNotEmpty, IsString, IsStrongPassword, Length, Matches, MaxLength } from "class-validator";
+import { IsCustomEmail, IsCustomPassword } from "src/common/decorators";
 
 export class AuthLoginDto {
-
-     @IsEmail()
-     @IsString()
-     @MaxLength(150)
-     @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
+     @IsCustomEmail()
      email: string;
 
-     @IsNotEmpty()
-     @Length(8, 64)
-     @Transform(({ value }) => (typeof value === 'string' ? value.trim() : value))
-     @Matches(/^\S*$/, { message: 'Password should not contain spaces.' })
-     @Matches(/^(?=.*[a-zA-Z])(?=.*[0-9]).*$/, { message: 'password must contain letters and numbers.' })
+     @IsCustomPassword()
      password: string;
-
 }
